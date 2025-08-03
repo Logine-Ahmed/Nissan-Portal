77a21a750427a546dd1a26ba858e5b3fb58ca71e
Logine-Ahmed <logineelshazly@gmail.com>
2025-08-03 22:10:37 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const cacheName = "nissan-scm-cache-v1";
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/logo.png',
+  '/nissan-car.jpeg',
+  '/manifest.json',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js'
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
